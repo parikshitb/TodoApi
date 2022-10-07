19f27e4552e101d0808003b8c6bff2f70a580a89
parikshit <parixit23@gmail.com>
2022-10-07 23:30:31 +0900

    microsoft leraning tutorial completion

---
+++ Models/TodoContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+	public class TodoContext : DbContext
+	{
+		//Override the OnConfiguring(DbContextOptionsBuilder) method
+		//OR
+		//DbContextOptionsBuilder<TContext> (or DbContextOptionsBuilder)
+		//OR
+		//externally create an instance of DbContextOptions<TContext> (or DbContextOptions)
+        //and pass it to a base constructor of DbContext
+		public TodoContext(DbContextOptions<TodoContext> contextOptions) : base(contextOptions)
+		{
+		}
+
+		//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/null-forgiving
+		public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+		
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,25 +1,31 @@
+﻿using TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+//following line is called to map attribute routed controllers(instead of conventional routing)
 app.MapControllers();
 
 app.Run();
